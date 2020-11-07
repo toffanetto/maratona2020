d562b7b31d128ba47cf67e0f42ddf73eb9c72dc2
Toffanetto <gabrieltoffanetto@gmail.com>
2020-11-07 14:56:04 -0300

    Update

--- template.cpp
+++ template.cpp
@@ -1,14 +1,19 @@
-/*  @toffanetto
+/*  goldStack
+*   Emanuel Alves da Silva E Souza
 *   Gabriel Toffanetto França da Rocha
+*   Willian Ferraz Gobira
 *   Universidade Federal de Itajubá - Campus Itabira
-*   
+*   Maratona de Programação -- 2020 -- 1a fase
 */
 
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main(){
 
+int main(){
+    
+ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
 }
